88b6dcb0b489ad1f4470de6a08d6a150657a8b76
faisn <86128055+faisn@users.noreply.github.com>
2022-06-23 15:51:54 -0700

    Add git ignore

--- config.js
+++ config.js
@@ -1,2 +1,23 @@
 console.log("config.js");
 console.log("Version 1");
+
+// console.log("Create a Version")
+console.log("git init")
+console.log("git status")
+console.log("git add {file name}");
+console.log("git commit -m  " + "enter the msg for the commit");
+console.log("git log")
+
+
+//View Previous Versions
+console.log("git checkout {commit hash the number that appear above every version history}");
+console.log("git checkout {branch name}")
+console.log("git log --all --graph" + "to see the graphical view of the versions")
+
+
+//Restore to Pervious Version
+console.log("git checkout <hash|branch> <file|folder>")
+console.log("git commmit -m  " + "msg")
+
+
+
